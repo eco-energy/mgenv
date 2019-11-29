e2fde47b393183e2b08f42f728da6be16a987379
Faez Shakil <faez@ecoenergyfinance.org>
2019-11-29 14:38:21 +0500

    coordinate system work in progress

--- src/Grid.hs
+++ src/Grid.hs
@@ -1,60 +1,58 @@
-{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE GADTs #-}
+
 
 module Grid where
 
+import Streamly
+import qualified Streamly.Prelude as S
+import Control.Monad.State
+import Control.Monad.Bayes.Class
+import Algebra.Graph.Labelled
+
 import GHC.Generics (Generic)
-import Physics.Storage (mkBattery)
-import Physics.Units (GeoC, R)
-import Node
-import Data.MemoTrie
-import GHC.Generics (Generic)
-import Node
+
+import Physics.Units (GeoC, R, Meters)
+import HH
+import Physics.Transmission
 
 type Theta = R
 type Gamma = R
-
-
-mkNode = Node
-
-data BatterySpec = BatterySpec
-  { tEnergy :: R
-  , cEff :: R
-  , dEff :: R
-  } deriving (Eq, Show, Ord, Generic)
-
-data LoadSpec = LoadSpec
-  { lpower :: R } deriving (Eq, Show, Ord, Generic)
-
-data PanelSpec = PanelSpec
-  { conversionEff :: R  } deriving (Eq, Show, Ord, Generic) 
-
-data TransmissionSpec = TransmissionSpec
-  { transmissionEfficiency :: R } deriving (Eq, Show, Ord, Generic)
  
 type GridPoints = (R, Theta, Gamma)
 
-type Loc = GeoC
-
 data GridSpec = GridSpec
-  { nNodes :: Int
-  , loadS :: [LoadSpec]
-  , panelS :: [PanelSpec]
-  , batteryS :: [BatterySpec]
-  , transmission :: [TransmissionSpec]
+  { centerPoint :: GeoC
+  , nNodes :: Int
   , nodeDistanceMean :: R
   , nodeDistanceStd :: R
   } deriving (Eq, Show, Generic)
 
+sampleGridSpec :: MonadSample m => m GridSpec
+sampleGridSpec = do
+  let
+    cp = (24.54743, 67.62771) :: GeoC
+  nNodes <- uniformD [10..100]
+  nodeDistanceMean <- uniform 10 50
+  nodeDistanceStd <- uniform 10 20
+  return $ GridSpec cp nNodes nodeDistanceMean nodeDistanceStd
+
+type Grid = Graph HHSpec TransmissionSpec
 
 
+mkGrid :: (MonadSample m) => GridSpec -> Grid
+mkGrid GridSpec {..} = do
+  nodeDistances <- replicateM nNodes $ normal nodeDistanceMean nodeDistanceStd
+  nodeAngles <- replicateM nNodes $ uniform 0 360
+  let
+    geoCs = map $ atDistanceAndAngle cp $ zip nodeDistances nodeAngles 
+    gridLocs = gridWithCenterAndPoints cp $ zip nodeDistances nodeAngles 
+  nodes <- mapM sampleHH nodeDistances
+  return Grid
 
-genNodes :: GridSpec -> [Node]
-genNodes GridSpec {..} =  [mkNode $ i s c g | i <- range nNodes]
-  where
-    mkNode = Node
-    s = mkBattery $ sample batteryS
-    c = mkLoad $ sample loadS
-    g = mkGen $ sample panelS
+
+gridWithCenterAndPoints :: GeoC -> [(Meters, Theta)] -> [(Meters, Meters)]
+gridWithCenterAndPoints = undefined
+
+atDistanceAndAngle :: GeoC -> Meters -> Theta -> GeoC
+atDistanceAndAngle = undefined
